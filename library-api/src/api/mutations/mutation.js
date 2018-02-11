ce97295c906f0fa5e7b783c68ad49c9567b26183
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-11 18:37:04 +0800

    Added Cancel Reservation API

--- library-api/src/api/mutations/mutation.js
+++ library-api/src/api/mutations/mutation.js
@@ -3,6 +3,7 @@ import LogInObject from "./mutationObjects/logIn";
 import AddUserObject from "./mutationObjects/addUser";
 import BorrowBookObject from "./mutationObjects/borrowBook";
 import ReserveBookObject from "./mutationObjects/reserveBook";
+import UnreserveBookObject from "./mutationObjects/unreserveBook";
 
 process.env.SECRET_KEY = "This is a test for the super secret key";
 
@@ -14,7 +15,8 @@ export default new GraphQLObjectType({
             logIn: LogInObject,
             addUser: AddUserObject,
             borrowBook: BorrowBookObject,
-            reserveBook: ReserveBookObject
+            reserveBook: ReserveBookObject,
+            unreserveBook: UnreserveBookObject
         };
     }
 });
---
+++ library-api/src/api/mutations/mutationObjects/unreserveBook.js
@@ -0,0 +1,78 @@
+import {
+    GraphQLString,
+    GraphQLInt,
+    GraphQLNonNull
+} from "graphql";
+import GraphQLJSON from "graphql-type-json";
+import DB from "../../../db/dbMap";
+import JWT from "jsonwebtoken";
+
+// TODO: Refactor
+
+export default {
+    type: GraphQLJSON,
+    description: "Cancels a reservation",
+    args: {
+        bookId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: "The book id that has a reservation"
+        },
+        token: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: "The token of the user that needs to cancel a reservation"
+        }
+    },
+    resolve(root, args) {
+        return JWT.verify(args.token, process.env.SECRET_KEY, null, (err, decoded) => {
+            if(err || !decoded) {
+                return {
+                    success: false,
+                    iat: Date.now(),
+                    reason: "Invalid Token"
+                }
+            }
+            else {
+                return DB.models.books.findOne({
+                    where: {
+                        id: args.bookId
+                    }
+                })
+                .then(book => {
+                    if(typeof book === 'undefined' || book === null) {
+                        return {
+                            success: false,
+                            iat: Date.now(),
+                            reason: "Book does not exist"
+                        }
+                    }
+                    else if(book.userId === null && book.userId !== decoded.userId) {
+                        return {
+                            success: false,
+                            iat: Date.now(),
+                            reason: "Book is not reserved to the user"
+                        }
+                    }
+                    else {
+                        book.update({
+                            userId: null
+                        });
+
+                        return DB.models.transactions.create({
+                            transactionType: "UNRESERVE BOOK",
+                            transactionRemarks: `user#${decoded.userId} cancels reservation to book#${args.bookId}`,
+                            userId: decoded.userId,
+                            bookId: args.bookId
+                        })
+                        .then(transaction => {
+                            return {
+                                success: true,
+                                iat: Date.now(),
+                                transactionID: transaction.id 
+                            }
+                        })
+                    }
+                })
+            }
+        });
+    }
+}
