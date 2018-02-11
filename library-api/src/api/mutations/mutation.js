350ce2883aaae8d27297ec639dac8735890bc7aa
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-11 15:39:09 +0800

    Added the reserveBook API

--- library-api/src/api/mutations/mutation.js
+++ library-api/src/api/mutations/mutation.js
@@ -2,6 +2,7 @@ import { GraphQLObjectType } from "graphql";
 import LogInObject from "./mutationObjects/logIn";
 import AddUserObject from "./mutationObjects/addUser";
 import BorrowBookObject from "./mutationObjects/borrowBook";
+import ReserveBookObject from "./mutationObjects/reserveBook";
 
 process.env.SECRET_KEY = "This is a test for the super secret key";
 
@@ -12,7 +13,8 @@ export default new GraphQLObjectType({
         return {
             logIn: LogInObject,
             addUser: AddUserObject,
-            borrowBook: BorrowBookObject
+            borrowBook: BorrowBookObject,
+            reserveBook: ReserveBookObject
         };
     }
 });
---
+++ library-api/src/api/mutations/mutationObjects/reserveBook.js
@@ -0,0 +1,73 @@
+import {
+    GraphQLString,
+    GraphQLInt,
+    GraphQLNonNull
+} from "graphql";
+import GraphQLJSON from "graphql-type-json";
+import DB from "../../../db/dbMap";
+import JWT from "jsonwebtoken";
+
+export default {
+    type: GraphQLJSON,
+    description: "Marks a book reserved",
+    args: {
+        bookId: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: "The id of the book the user wants to reserve"
+        },
+        token: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: "The token of the user that wants to reserve"
+        }
+    },
+    resolve(root, args) {
+        // TODO: Refactor, code too long
+        return JWT.verify(args.token, process.env.SECRET_KEY, null, (err, decoded) => {
+            
+            // Check if theres no error during decryption
+            if(err || !decoded) {
+                return {
+                    success: false,
+                    ita: Date.now(),
+                    reason: "Invalid Token"
+                }
+            }
+            else {
+                return DB.models.books.findOne({
+                    where: {
+                        id: args.bookId
+                    }
+                })
+                .then(book => {
+                    // Check if the book isn't reserved or borrowed by anyone
+                    if(book.userId === null) {
+                        book.update({
+                            userId: decoded.userId
+                        });
+
+                        return DB.models.transactions.create({
+                            transactionType: "RESERVING BOOK",
+                            transactionRemark: `userId ${decoded.userId} reserves bookId ${args.bookId}`,
+                            bookId: args.bookId,
+                            userId: decoded.userId
+                        })
+                        .then((transaction) => {
+                            return {
+                                success: true,
+                                ita: Date.now(),
+                                transactionId: transaction.id
+                            }
+                        })
+                    } 
+                    else {
+                        return {
+                            success: false,
+                            ita: Date.now(),
+                            reason: "Book is borrowed or reserved by somebody"
+                        }
+                    }
+                })
+            }
+        });
+    }
+};
