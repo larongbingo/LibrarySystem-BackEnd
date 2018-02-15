89a6fc307acac0042516245c830670f773fd16f6
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-15 15:36:22 +0800

    Changed the from NPM to RoR Jekyll

--- docs/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-[1, 2, 3].forEach(function (x) {
-    return showItWorks();
-});
-
-function showItWorks() {
-    document.getElementById("javascript-status").innerHTML = "JavaScript transpilation works!";
-}
