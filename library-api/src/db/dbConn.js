6df45d3f405f2016916115fba16540daae3a814c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-18 13:15:28 +0800

    Added the credentials for the mysql db

--- library-api/src/db/dbConn.js
+++ library-api/src/db/dbConn.js
@@ -5,7 +5,8 @@
 
 import Sequelize from "sequelize";
 
-export default new Sequelize("librarySystems", "root", "root", {
-    host: "localhost",
+export default new Sequelize("LibrarySystems", "admin", "b0bski123", {
+    host: "sl-us-south-1-portal.19.dblayer.com",
+    port: "35234",
     dialect: "mysql"
 });
