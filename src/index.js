b4cb327d4f8041381a562c79f4e75c455be44e92
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-18 18:00:01 +0800

    Added a message at '/' route

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ App.use(CORS());
 // Log all HTTP Requests to console
 App.use(Morgan("combined"));
 
+App.use("/", function(req, res, next) {
+    res.send("<h1>This is the API server of the Library System</h1>")
+});
+
 // Route for the GraphQLHTTP middleware
 App.use("/api", GraphqlHTTP({
     schema: API_Schema
