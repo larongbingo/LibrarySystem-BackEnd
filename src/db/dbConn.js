d0d8d33d6e1306fe570506510236aad242a05366
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-19 22:01:54 +0800

    Added the handling of production and development environments

--- src/db/dbConn.js
+++ src/db/dbConn.js
@@ -5,8 +5,19 @@
 
 import Sequelize from "sequelize";
 
-export default new Sequelize("LibrarySystems", "admin", "b0bski123", {
-    host: "sl-us-south-1-portal.19.dblayer.com",
-    port: "35234",
-    dialect: "mysql"
-});
+let conn;
+
+if(process.env.NODE_ENV === "production") {
+    conn = new Sequelize("LibrarySystems", "admin", "b0bski123", {
+        host: "sl-us-south-1-portal.19.dblayer.com",
+        port: "35234",
+        dialect: "mysql"
+    });
+}
+else {
+    conn = new Sequelize("LibrarySystems", "root", "root", {
+        dialect: "mysql"
+    });
+}
+
+export default conn;
