6a8c6ec41a04d3ecf1a48248b3b01341087e467f
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-21 12:58:03 +0800

    Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        "env",
+                        ["es2015", {"modules": false}]
+                    ]
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            parallel: {
+                cache: true,
+                workers: true
+            },
+            cache: true,
+            sourceMap: true,
+            uglifyOptions: {
+                ecma: 5,
+                compress: true,
+                mangle: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+        })
+    ],
+    target: "node",
+    cache: true,
+    stats: "verbose", 
+    devtool: "source-map",
+    externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore']
+}
