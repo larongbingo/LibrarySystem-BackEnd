05e1995fe7ad2390bfdb6b90552180e5c45b6377
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-26 07:46:12 +0800

    Changed the query operators

--- src/api/queries/queryObjects/transactions.js
+++ src/api/queries/queryObjects/transactions.js
@@ -10,7 +10,19 @@ import {
     GraphQLList
 } from "graphql";
 import TransactionsObject from "../../tables/transactions";
-import DB from "../../../db/dbMap";   
+import DB from "../../../db/dbMap";
+import { Op } from "sequelize";
+
+function percentify(str) {
+    return '%' + str + '%';
+}
+
+const FIELDS = [
+    ["id", Op.eq],
+    ["transactionType", Op.like, percentify],
+    ["userId", Op.eq],
+    ["bookId", Op.eq]
+];
 
 export default {
     description: "Returns a list of transactions",
@@ -34,6 +46,18 @@ export default {
         }
     },
     resolve(root, args) {
-        return DB.models.transactions.findAll({where: args});
+        let query = {}
+
+        FIELDS.forEach(element => {
+            if(args[element[0]]) {
+                query[element[0]] = {
+                    [element[1]]: (element[2]) ? (element[2])(args[element[0]]) : args[element[0]]
+                }
+            }
+        });
+
+        console.log(query);
+
+        return DB.models.transactions.findAll({where: query});
     }
 }
