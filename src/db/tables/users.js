a29f3aaac885c2360d417e93bc2047a198dc83ff
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-25 10:36:26 +0800

    Changed the hook from beforeCreate to beforeValidate

--- src/db/tables/users.js
+++ src/db/tables/users.js
@@ -8,34 +8,36 @@ import { hashSync } from "bcryptjs";
 import DB from "../dbConn";
 
 const Users = DB.define("users", {
-    firstName: {
-        type: STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: STRING,
-        allowNull: false
-    },
-    userID: {
-        type: STRING,
-        allowNull: false
-    },
-    userType: {
-        type: STRING,
-        allowNull: false
-    },
-    username: {
-        type: STRING,
-        allowNull: false
-    },
-    password: {
-        type: STRING,
-        allowNull: false
-    }
-}, {
+        firstName: {
+            type: STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: STRING,
+            allowNull: false
+        },
+        userID: {
+            type: STRING,
+            allowNull: false
+        },
+        userType: {
+            type: STRING,
+            allowNull: false
+        },
+        username: {
+            type: STRING,
+            allowNull: false
+        },
+        password: {
+            type: STRING,
+            allowNull: false
+        }
+    }, 
+    {
     hooks: {
-        beforeCreate: (user, options) => {
-            user.password = hashSync(user.password);
+        beforeValidate: (user, options) => {
+            if(user.password)
+                user.password = hashSync(user.password);
         }
     }
 });
