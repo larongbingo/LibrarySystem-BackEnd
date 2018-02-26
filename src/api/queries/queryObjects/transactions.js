af8bbd05f9ef2a6bad7b10cb62759815b0aa1acb
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-26 08:53:35 +0800

    Added the verification of Token at Transaction API

--- src/api/queries/queryObjects/transactions.js
+++ src/api/queries/queryObjects/transactions.js
@@ -7,11 +7,14 @@ import {
     GraphQLString, 
     GraphQLInt,
     GraphQLObjectType,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } from "graphql";
 import TransactionsObject from "../../tables/transactions";
 import DB from "../../../db/dbMap";
+import JWT from "jsonwebtoken";
 import { Op } from "sequelize";
+import STATUS_MSG from "../../mutations/mutationObjects/helpers/statusCodes";
 
 function percentify(str) {
     return '%' + str + '%';
@@ -28,6 +31,10 @@ export default {
     description: "Returns a list of transactions",
     type: new GraphQLList(TransactionsObject),
     args: {
+        token: {
+            description: "The token of the staff or admin",
+            type: new GraphQLNonNull(GraphQLString)
+        },
         id: {
             description: "The ID in the Database",
             type: GraphQLInt
@@ -48,16 +55,35 @@ export default {
     resolve(root, args) {
         let query = {}
 
-        FIELDS.forEach(element => {
-            if(args[element[0]]) {
-                query[element[0]] = {
-                    [element[1]]: (element[2]) ? (element[2])(args[element[0]]) : args[element[0]]
-                }
+        return JWT.verify(args.token, process.env.SECRET_KEY, (err, decoded) => {
+            if(err || !decoded) {
+                return [{transactionRemarks: JSON.stringify(STATUS_MSG["3"])}];
+            }
+            else {
+                return DB.models.sessions.findOne({
+                    where: {
+                        token: args.token
+                    }
+                })
+                .then(session => {
+                    if(!session) {
+                        return [{transactionRemarks: JSON.stringify(STATUS_MSG["4"])}];
+                    }
+                    else {
+                        FIELDS.forEach(element => {
+                            if(args[element[0]]) {
+                                query[element[0]] = {
+                                    [element[1]]: (element[2]) ? (element[2])(args[element[0]]) : args[element[0]]
+                                }
+                            }
+                        });
+                
+                        console.log(query);
+                
+                        return DB.models.transactions.findAll({where: query});
+                    }
+                })
             }
         });
-
-        console.log(query);
-
-        return DB.models.transactions.findAll({where: query});
     }
 }
