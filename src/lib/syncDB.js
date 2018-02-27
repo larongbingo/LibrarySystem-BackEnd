8b0aa15eb95481d74c8a92c71b5417ffdb8ed9fa
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-02-27 18:17:01 +0800

    Removed unnecessary code

--- src/lib/syncDB.js
+++ src/lib/syncDB.js
@@ -5,41 +5,18 @@
  */
 
 import DB from "../db/dbMap";
-import UsersTable from "../db/tables/users";
-import BooksTable from "../db/tables/books";
-import TransactionsTable from "../db/tables/transactions";
 
-DB.sync({force: true}).then(() => {
-    return UsersTable.create({
+DB.sync({force: true})
+.then(() => {
+    return DB.models.users.create({
         firstName: 'admin', 
         lastName: 'admin',
         userID: 'ADMIN0001',
         userType: "ADMINISTRATOR",
         username: "admin",
         password: "admin"
-    }).then((user) => {
-        return BooksTable.create({
-            title: "Testing Book",
-            author: "Testing Author",
-            ISBN: "TESTING-ISBN-TEST",
-            isBorrowed: false
-        })
-        .then(book => {
-            if(process.env.NODE_ENV === "production")
-                book.destroy();
-        })
-    }).then(() => {
-        return TransactionsTable.create({
-            transactionType: "TESTING",
-            transactionRemarks: "This is a test"
-        })
-        .then(transaction => {
-            if(process.env.NODE_ENV === "production")
-                transaction.destroy();
-        })
-    }).then(() => {
-        DB.close();
-        console.log('exiting')
-        process.exit(0);
-    })
-});
+    });
+})
+.then(() => {
+    process.exit(0);
+})
