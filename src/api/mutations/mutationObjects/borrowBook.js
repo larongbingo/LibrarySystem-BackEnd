cf7533011682d706bd191c933c13deabc51b39d2
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-05 14:47:35 +0800

    Refactored borrowBook API

--- src/api/mutations/mutationObjects/borrowBook.js
+++ src/api/mutations/mutationObjects/borrowBook.js
@@ -12,6 +12,8 @@ import GraphQLJSON from "graphql-type-json";
 import DB from "../../../db/dbMap";
 import JWT from "jsonwebtoken";
 import createResponse from "./helpers/createResponse";
+import verifyAccount from "./helpers/accountVerifier";
+import STATUS_MSG from "./helpers/statusCodes";
 
 export default {
     type: GraphQLJSON,
@@ -35,60 +37,56 @@ export default {
         }
     },
     resolve(root, args) {
-        return DB.models.books.findOne({
-            where: {
-                id: args.bookId
+        return verifyAccount(args.token)
+        .then(data => {
+            if(data.status_code === 0) {
+                if(!data.isAdminOrStaff) {
+                    return STATUS_MSG["8"];
+                }
+
+                return DB.models.books.findOne({
+                    where: {
+                        id: args.bookId
+                    }
+                })
+                .then(book => {
+                    // Check if the book exists in the database
+                    if(!book) {
+                        return STATUS_MSG["17"];
+                    }
+
+                    // Check if the book is not borrowed or reserved by anyone
+                    else if(book.userId || book.isBorrowed) {
+                        return STATUS_MSG["13"];
+                    }
+
+                    else {
+                        // Update Book
+                        book.update({
+                            isBorrowed: true,
+                            userId: args.userId
+                        });
+
+                        // Create transaction object
+                        return DB.models.transactions.create({
+                            transactionType: "BORROWING BOOK",
+                            transactionRemarks: args.transactionRemark,
+                            userId: args.userId,
+                            bookId: args.bookId
+                        })
+                        .then(transaction => {
+                            return createResponse(true, 0, {
+                                transactionID: transaction.id,
+                                transactionType: transaction.transactionType,
+                                transactionRemarks: transaction.transactionRemarks
+                            });
+                        });
+                    }
+                });
             }
-        })
-        .then(book => {
-            return JWT.verify(args.token, process.env.SECRET_KEY, null, (err, decoded) => {
-                if(err || !decoded) {
-                    return createResponse(false, 3, {reason: "Invalid Token"});
-                    // TODO: Refactor everything
-                }
-                else if(decoded.position === 'ADMINISTRATOR' || decoded.position === "STAFF") {
-                    return DB.models.sessions.findOne({
-                        where: {
-                            token: args.token
-                        }
-                    })
-                    .then(session => {
-                        if(!session) {
-                            return createResponse(false, 3, {reason: "Token is expired"});
-                        }
-
-                        if(book !== null && typeof book !== 'undefined' && !book.isBorrowed && book.userId === null) {
-                            // Update the book
-                            book.update({
-                                isBorrowed: true,
-                                userId: args.userId
-                            });
-            
-                            // Create a transaction object to transactions table
-                            return DB.models.transactions.create({
-                                transactionType: "BORROWING BOOK",
-                                transactionRemarks: args.transactionRemark,
-                                userId: args.userId,
-                                bookId: args.bookId
-                            })
-                            .then(transaction => {
-                                return createResponse(true, 0, {
-                                    transactionID: transaction.id,
-                                    transactionType: transaction.transactionType,
-                                    transactionRemarks: transaction.transactionRemarks
-                                })
-                            });
-                        }
-                        else {
-                            return createResponse(false, 7, {reason: "Book is currently lended to someone"});
-                        }
-                    })
-                }
-                else {
-                    return createResponse(false, 8, {reason: "The borrowing of a book needs to be validated by a STAFF or an ADMIN"}); 
-                }
-            })
-
+            else {
+                return STATUS_MSG[String(data)];
+            }
         });
     }
 }
