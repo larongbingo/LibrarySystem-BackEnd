cca5a906b95d36a6605e21fbd0e7a0dc674da577
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-05 15:19:39 +0800

    Added notes to the deprecated responses

--- src/api/mutations/mutationObjects/helpers/statusCodes.js
+++ src/api/mutations/mutationObjects/helpers/statusCodes.js
@@ -7,13 +7,13 @@ const STATUS_CODES = {
     4: createResponse(false, 4, {reason: "Expired Token"}),
     5: createResponse(false, 5, {reason: "Invalid userType"}),
     6: createResponse(false, 6, {reason: "Username already taken"}),
-    7: createResponse(false, 7, {reason: "Book is currently lended to someone"}),
+    7: createResponse(false, 7, {reason: "Book is currently lended to someone", message: "This message is deprecated!"}), // DEPRECATED!!!
     8: createResponse(false, 8, {reason: "Borrowing of a book must be validated by a staff or admin"}),
     9: createResponse(false, 9, {reason: "User does not exist"}),
     10: createResponse(false, 10, {reason: "Token ID is not valid"}),
     11: createResponse(false, 11, {reason: "Wrong username or password"}),
     12: createResponse(false, 12, {reason: "User is not logged in"}),
-    13: createResponse(false, 13, {reason: "Book is borrowed or reserved by someone"}),
+    13: createResponse(false, 13, {reason: "Book is borrowed or reserved by someone"}), // Use #13 instead of #7 for books that is borrowed or reserved to someone
     14: createResponse(false, 14, {reason: "Book is not borrowed by the requestee"}),
     15: createResponse(false, 15, {reason: "Book is not yet borrowed"}),
     16: createResponse(false, 16, {reason: "Returning of a book must be validated by a staff or admin"}),
