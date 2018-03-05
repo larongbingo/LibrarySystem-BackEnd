de741590c9a83c3b7e2e6e0ef59a4df600f46f00
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-05 15:18:51 +0800

    Refactored changeAccountInfo API

--- src/api/mutations/mutationObjects/changeAccountInfo.js
+++ src/api/mutations/mutationObjects/changeAccountInfo.js
@@ -7,6 +7,7 @@ import DB from "../../../db/dbMap";
 import JWT from "jsonwebtoken";
 import createResponse from "./helpers/createResponse";
 import STATUS_MSG from "./helpers/statusCodes";
+import verifyAccount from "./helpers/accountVerifier";
 
 const ARG_FIELDS = [
     ["newFirstName", "firstName"],
@@ -47,68 +48,54 @@ export default {
         }
     },
     resolve(root, args) {
-        return JWT.verify(args.token, process.env.SECRET_KEY, (err, decoded) => {
-            // Check if the token is valid
-            if(err || !decoded) {
-                return STATUS_MSG["3"];
-            }
-            else {
-                return DB.models.sessions.findOne({
-                    where: {
-                        token: args.token
-                    }
-                })
-                .then(session => {
-                    // Check if the token has a valid session
-                    if(!session) {
-                        return STATUS_MSG["4"];
-                    }
-                    else {
-                        // Reset the log of changed fields
-                        updatedFields = "";
+        return verifyAccount(args.token)
+            .then(data => {
+                if (data.status_code === 0) {
+                    // Reset the log of changed fields
+                    updatedFields = "";
 
-                        ARG_FIELDS.forEach((element) => {
-                            // Check all of the non null fields in the args
-                            if(args[element[0]]) {
-                                updatedFields += element[0] + ", ";
+                    ARG_FIELDS.forEach((element) => {
 
-                                console.log(`{"${element[1]}": "${args[element[0]]}"}`);
+                        // Check all of the non null fields in the args
+                        if (args[element[0]]) {
+                            updatedFields += element[0] + ", ";
 
-                                // Update all of the values
-                                DB.models.users.update(
-                                    JSON.parse(`{"${element[1]}": "${args[element[0]]}"}`), 
-                                    {
-                                        where:{
-                                            id: decoded.userId
-                                        }
-                                    },
-                                    {individualHooks: true}
-                                )
-                                .then(() => {});
-                            }
-                        })
+                            // Update all of the values
+                            DB.models.users.update(
+                                JSON.parse(`{"${element[1]}": "${args[element[0]]}"}`), {
+                                    where: {
+                                        id: data.data.userId
+                                    }
+                                }, {
+                                    individualHooks: true
+                                }
+                            )
+                            .then(() => {});
+                        }
+                    })
 
-                        // Store to DB the changed fields
-                        DB.models.transactions.create({
-                            transactionType: "CHANGE ACCOUNT INFO",
-                            transactionRemarks: "Changed the following details: " + updatedFields,
-                            userId: decoded.userId
-                        })
-                        
-                        // Delete current token of user from sessions
-                        DB.models.sessions.destroy({
-                            where: {
-                                token: args.token
-                            }
-                        })
+                    // Store to DB the changed fields
+                    DB.models.transactions.create({
+                        transactionType: "CHANGE ACCOUNT INFO",
+                        transactionRemarks: "Changed the following details: " + updatedFields,
+                        userId: data.data.userId
+                    })
 
-                        // Send to client of the result
-                        return createResponse(true, 0, {
-                            updatedFields: updatedFields
-                        })
-                    }
-                })
-            }
-        })
+                    // Delete current token of user from sessions
+                    DB.models.sessions.destroy({
+                        where: {
+                            token: args.token
+                        }
+                    })
+
+                    // Send to client of the result
+                    return createResponse(true, 0, {
+                        updatedFields: updatedFields
+                    })
+                }
+                else {
+                    return STATUS_MSG[String(data)];
+                }
+            })
     }
 }
