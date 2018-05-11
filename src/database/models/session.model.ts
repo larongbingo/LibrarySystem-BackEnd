a9af0dd1928d1941bbc247025680772d19b7a790
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-05-11 09:45:06 +0800

    Added license to session model

--- src/database/models/session.model.ts
+++ src/database/models/session.model.ts
@@ -1,3 +1,21 @@
+/*****************************************************************************
+ *  The backend of cvsu library app, handles data storage and processing
+ *  Copyright (C) 2018  Renz Christen Yeomer A. Pagulayan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
 import { Model, Column, Table, DataType, AllowNull } from "sequelize-typescript";
 import * as Bluebird from "bluebird";
 import { compare } from "bcryptjs";
@@ -35,7 +53,7 @@ export class Session extends Model<Session> {
                         resolve(null);
                         return;
                     }
-                    
+
                     resolve(sign(user, process.env.KEY));
                 })
             })
