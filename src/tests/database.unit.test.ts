03e51ee0b047e364e35564c578006c85947d0cc1
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-05-14 15:11:14 +0800

    Added Unit Testing

---
+++ src/tests/database.unit.test.ts
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ *  The backend of cvsu library app, handles data storage and processing
+ *  Copyright (C) 2018  Renz Christen Yeomer A. Pagulayan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+import sequelize from "database";
+
+describe("Database Unit Tests", function() {
+    before("Sync Tables and Models", function() {
+        return sequelize.sync({ force: true });
+    });
+
+    
+});
